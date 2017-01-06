598c1ad6c7ce8fa8180a13f61daf6df6dd813f97
张俊飞 <zhangjunfei@zhujia360.com>
2017-01-06 15:50:36 +0800

    Initial Commit

---
+++ ZJYCeShiDetailServiceProtocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJYCeShiDetailServiceProtocol
+//
+//  Created by apple on 2017/1/6.
+//  Copyright © 2017年 JunFly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJYCeShiDetailServiceProtocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJYCeShiDetailServiceProtocol
+//
+//  Created by apple on 2017/1/6.
+//  Copyright © 2017年 JunFly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
